69912bb17b50dda51cb7a76a191f81b5f4ec957e
Apeksha-borde <bordeapeksha@gmail.com>
2026-02-11 18:44:22 +0530

    first change

---
+++ script.js
@@ -0,0 +1,285 @@
+// var age =Number(prompt("Enter tour age"))
+
+// if (age>=18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("tou cant vote")
+// }
+
+
+// Binary
+
+
+// && (*)
+
+// 0   0    =   0  
+
+// 1   0    =   0
+
+// 0   1    =   0
+
+// 1   1   =    1
+
+
+
+// || (+)
+
+// 0   0   =   0
+
+// 0   1   =   1
+
+// 1   0   =   1
+
+// 1   1   =   1
+
+
+
+// var age=20
+
+// if(age>18 && age>60){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("no");
+// }
+
+
+
+
+
+
+// var a = 10
+// var b=  "10"
+
+// if (a===b){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+
+
+
+// var units=Number(prompt("enter your number"))
+
+// var bill
+
+// if (units>=100){
+//     bill=units*10
+// }
+// else if (units>50){
+//     bill=units*8
+// }
+// else{
+//     bill=units*5
+// }
+// console.log(bill);
+
+
+
+
+
+
+
+
+
+// const student={
+
+//     fulname:"apeksha",
+//     age:20,
+//     cgpa:8.45,
+//     ispass:true,
+
+// };
+
+// student["age"]=student["age"] + 1
+
+// student["ispass"]=false;
+// console.log(student["ispass"]);
+// console.log(student["cgpa"]);  
+// console.log(student["fulname"]);
+// console.log(student["ispass"]);
+
+
+
+
+
+
+
+// const product={
+
+//     title:"ball pen",
+//     rating:4,
+//     offer:5,
+//     price:270,
+// }
+
+
+// console.log(product); 
+
+
+
+
+
+
+// we can update let variable 
+
+// not update const variable 
+
+// but in object of const we update keyof a object
+
+
+
+
+
+
+// const profile={
+//     username:"apeksha",
+//     isfollow:false,
+//     following:100,
+//     followers:100,
+// };
+
+// console.log(typeof profile[(following)]);
+ 
+
+
+// console.log("print hello");
+
+
+// let a= 6;
+// let b=7;
+
+// console.log("a=",a,"& b=",b);
+
+// a=a+1;
+// // console.log(a);
+
+
+// let a=7;
+// let b=5;
+
+// a++;
+// console.log("a++","=",a++);
+
+
+
+
+// comparision operator
+
+// let a=10;
+// let b=9;
+
+// console.log("10<9",a>b);
+
+
+
+//  let num = 10;
+
+// if (num%2===0){
+//     console.log(num,"is even")
+// }
+// else{
+//     console.log(num,"is odd")
+// }
+
+
+
+// let mode ="blue";
+// let color;
+
+// if (mode === "dark"){
+//     color="black";
+// }else if (mode === "blue"){
+//     color="blue";
+// }else {
+//     color="white";
+// }
+
+// console.log(color);
+
+
+
+// let sum = 0;
+
+// for(let i=1;i<=5;i++){
+//     sum = sum+i;
+// }
+
+// console.log("sum =",sum);
+
+
+
+// let i=0;
+
+// for (let i=1;i<=8;i++){
+//      console.log(i);
+// }
+
+
+
+// let  i=1;
+
+// while(i<=5){
+//     console.log("apeksha")
+//     i++;
+// }
+
+
+
+
+// let i=20;
+
+// do{
+//     console.log("apeksha");
+// }while(i<=10);
+ 
+
+
+// let i=100;
+
+// for (i=0;i<=100;i=i+2){
+//     console.log(i);
+// }
+
+
+// for (i=0;i<=100;i++){
+//     if(i%2!=0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// let gamenumber=25;
+
+// let usernumber=prompt("give me your number");
+
+// while(usernumber!=gamenumber){
+//     usernumber=prompt("you entered a  wrong number : enter again");
+// }
+// console.log("congratulation you enter a right number");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
