08b116f2dcc66dac0361635105a642baa8001da8
Apeksha Borde <bordeapeksha20@gmail.com>
2026-03-04 11:12:36 +0530

    execute a piece of code using loops

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const profile={
-     Name:"apeksha",
-     followers:40,
-     following:30,
-     isfollow:true,
+// const profile={
+//      Name:"apeksha",
+//      followers:40,
+//      following:30,
+//      isfollow:true,
 
+// }
+
+// console.log(profile);
+
+
+for(let i=1;i<=5;i++){
+     console.log("Apeksha");
 }
-
-console.log(profile);
 
 
 
