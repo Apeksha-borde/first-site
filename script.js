f6ea6637b5706cde3041a69bc47648554bcd2a2e
Apeksha Borde <bordeapeksha20@gmail.com>
2026-03-06 13:07:14 +0530

    start Array

--- script.js
+++ script.js
@@ -89,12 +89,17 @@
 
 
 
-let str = "madam";
+// let str = "madam";
 
-let reversed = str.split("").reverse().join("");
+// let reversed = str.split("").reverse().join("");
 
-if (str === reversed) {
-  console.log("Palindrome");
-} else {
-  console.log("Not Palindrome");
-}
+// if (str === reversed) {
+//   console.log("Palindrome");
+// } else {
+//   console.log("Not Palindrome");
+// }
+
+
+let fruits = ["Apple", "Banana", "Mango"];
+
+console.log(fruits);
