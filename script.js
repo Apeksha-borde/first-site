0e37e119b677d7ffdbd7b266ddde309782fc5179
Apeksha Borde <bordeapeksha20@gmail.com>
2026-03-07 23:54:10 +0530

    cover Array

--- script.js
+++ script.js
@@ -122,13 +122,35 @@
 // console.log(fruits.includes("Banana"));
 
 
-let arr = [1,2,3,4,5];
-let target = 5;
+// let arr = [1,2,3,4,5];
+// let target = 5;
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] + arr[j] === target) {
-      console.log(arr[i], arr[j]);
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[i] + arr[j] === target) {
+//       console.log(arr[i], arr[j]);
+//     }
+//   }
+// }
+
+
+
+
+
+function flatten(arr) {
+  let result = [];
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flatten(item));
+    } else {
+      result.push(item);
     }
   }
+
+  return result;
 }
+
+let arr = [1, [2, [3, 4]], 5];
+
+console.log(flatten(arr));
