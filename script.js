dd433463f5a381763b9ae53b76b0fa20f415d161
Apeksha Borde <bordeapeksha20@gmail.com>
2026-03-09 21:45:20 +0530

    implement dynamic todo list with add and delete task functionality using DOM manipulation

--- script.js
+++ script.js
@@ -235,9 +235,40 @@
 
 
 
-const title = document.getElementById("title");
-const btn = document.getElementById("btn");
+// const title = document.getElementById("title");
+// const btn = document.getElementById("btn");
 
-btn.addEventListener("click", function () {
-  title.textContent = "DOM Manipulation Working!";
+// btn.addEventListener("click", function () {
+//   title.textContent = "DOM Manipulation Working!";
+// });
+
+
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", function () {
+
+  const taskText = input.value;
+
+  if (taskText === "") {
+    alert("Please enter a task");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+
+  input.value = "";
+
 });
