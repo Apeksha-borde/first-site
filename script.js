53b00bd3ab32d9381c51db44823634fb1bd068d5
Apeksha Borde <bordeapeksha20@gmail.com>
2026-03-09 21:48:31 +0530

    implement image toggle using DOM manipulation and event listener

--- script.js
+++ script.js
@@ -243,32 +243,50 @@
 // });
 
 
-const input = document.getElementById("taskInput");
-const button = document.getElementById("addBtn");
-const list = document.getElementById("taskList");
+// const input = document.getElementById("taskInput");
+// const button = document.getElementById("addBtn");
+// const list = document.getElementById("taskList");
+
+// button.addEventListener("click", function () {
+
+//   const taskText = input.value;
+
+//   if (taskText === "") {
+//     alert("Please enter a task");
+//     return;
+//   }
+
+//   const li = document.createElement("li");
+//   li.textContent = taskText;
+
+//   const deleteBtn = document.createElement("button");
+//   deleteBtn.textContent = "Delete";
+
+//   deleteBtn.addEventListener("click", function () {
+//     li.remove();
+//   });
+
+//   li.appendChild(deleteBtn);
+//   list.appendChild(li);
+
+//   input.value = "";
+
+// });
+
+
+const image = document.getElementById("image");
+const button = document.getElementById("toggleBtn");
+
+let isFirstImage = true;
 
 button.addEventListener("click", function () {
 
-  const taskText = input.value;
+if (isFirstImage) {
+image.src = "https://via.placeholder.com/200/ff0000";
+} else {
+image.src = "https://via.placeholder.com/200";
+}
 
-  if (taskText === "") {
-    alert("Please enter a task");
-    return;
-  }
-
-  const li = document.createElement("li");
-  li.textContent = taskText;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete";
-
-  deleteBtn.addEventListener("click", function () {
-    li.remove();
-  });
-
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-
-  input.value = "";
+isFirstImage = !isFirstImage;
 
 });
